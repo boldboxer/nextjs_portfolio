4702f1bc2fc6e541ebdb6ec986099b8669ae76cc
Nezzer Mose <ebesteinpr@gmail.com>
2025-09-18 23:56:19 +0300

    next version errors fix 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/footer";
 import Header from "@/components/header";
 import { cn } from "@/lib/utils";
 import Script from "next/script"; // ✅ use Next.js Script
-import MatomoTracker from "@/components/matomo-tracker";
+// import MatomoTracker from "@/components/matomo-tracker";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -67,7 +67,7 @@ export default function RootLayout({
         <Providers>
           <Header />
           <main className="grow">
-            <MatomoTracker /> {/* ✅ Tracks SPA route changes */}
+            {/* <MatomoTracker /> ✅ Tracks SPA route changes */}
             {children}
           </main>
           <Footer />
--- app/unsubscribe/page.tsx
+++ app/unsubscribe/page.tsx
@@ -5,7 +5,6 @@ import UnsubscribeContent from '@/components/unsubscribe-content'
 
 export const dynamic = "force-dynamic"
 export const fetchCache = "force-no-store"
-export const revalidate = 0 // <— fully disables static prerendering
 
 
 
